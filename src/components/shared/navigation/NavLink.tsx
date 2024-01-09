430f8dc2cafc8d4fb3d50c30814efb18ca8f1016
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-01-09 23:21:54 +0100

    refactor: change current page selection

--- src/components/shared/navigation/NavLink.tsx
+++ src/components/shared/navigation/NavLink.tsx
@@ -1,27 +1,23 @@
-"use client"
 import Link from "next/link";
 import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
 
 interface NavLinkProps {
     href: string;
     name: string;
+    current?: boolean;
 }
 
-function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ')
-}
-
-const NavLink = ({href, name}: NavLinkProps) => {
-    const pathname = usePathname()
+const NavLink = ({href, name, current}: NavLinkProps) => {
 
     return (
         <Link
             href={href}
-            className={classNames(
-                pathname === href ? 'text-gray-900 border-b-2 border-primary' : 'text-gray-500 hover:text-gray-900',
-                "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+            className={cn(
+                "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 transform transition-all duration-300",
+                current ? 'text-gray-900 border-b-2 border-primary' : 'text-gray-500 hover:text-gray-900',
             )}
-            aria-current={pathname === href ? 'page' : undefined}
+            aria-current={current ? 'page' : undefined}
         >
             {name}
         </Link>
