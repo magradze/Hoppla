184a3c307220c2aa64efe86e70aa9e7b44d05aeb
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-01-19 18:43:21 +0100

    refactor: add my rides table

--- src/components/rides/RideLink.tsx
+++ src/components/rides/RideLink.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {getRideLinks} from "@/lib/actions/rides";
 import Link from "next/link";
+import moment from "moment";
 
 const RideLink = async () => {
 
@@ -9,8 +10,9 @@ const RideLink = async () => {
         <>
             {rideLinks?.slice(0, 6).map((ride: any, index: number) => (
                 <li key={index}>
-                    <Link href={`/search?from=${ride.rideFrom}&to=${ride.rideTo}&date=${ride.rideDate}&seats=1`}
-                          className="text-sm leading-6 text-gray-600 hover:text-gray-900 fira-go">
+                    <Link
+                        href={`/search?from=${ride.rideFrom}&to=${ride.rideTo}&date=${moment(new Date()).format("YYYY-MM-DD")}&seats=1`}
+                        className="text-sm leading-6 text-gray-600 hover:text-gray-900 fira-go">
                         {ride.rideName}
                     </Link>
                 </li>
