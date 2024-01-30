452d2517ece9c1e058ae7b37fbc6b44660c340e4
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-01-30 15:17:27 +0100

    chore: add companies and directions in Admin Panel

--- src/components/rides/RideLink.tsx
+++ src/components/rides/RideLink.tsx
@@ -4,24 +4,24 @@ import Link from "next/link";
 import moment from "moment";
 
 const RideLink = async () => {
-
-    const rideLinks = await getRideLinks()
-
-    if (rideLinks.length > 0) {
-        return (
-            <>
-                {rideLinks?.slice(0, 6).map((ride: any, index: number) => (
-                    <li key={index}>
-                        <Link
-                            href={`/search?from=${ride.rideFrom}&to=${ride.rideTo}&date=${moment(new Date()).format("YYYY-MM-DD")}&seats=1`}
-                            className="text-sm leading-6 text-gray-600 hover:text-gray-900 fira-go">
-                            {ride.rideName}
-                        </Link>
-                    </li>
-                ))}
-            </>
-        );
-    }
+    //
+    // const rideLinks = await getRideLinks()
+    //
+    // if (rideLinks.length > 0) {
+    //     return (
+    //         <>
+    //             {rideLinks?.slice(0, 6).map((ride: any, index: number) => (
+    //                 <li key={index}>
+    //                     <Link
+    //                         href={`/search?from=${ride.rideFrom}&to=${ride.rideTo}&date=${moment(new Date()).format("YYYY-MM-DD")}&seats=1`}
+    //                         className="text-sm leading-6 text-gray-600 hover:text-gray-900 fira-go">
+    //                         {ride.rideName}
+    //                     </Link>
+    //                 </li>
+    //             ))}
+    //         </>
+    //     );
+    // }
 
     return (
         <>
